d918c01234ec2f3f2c5362067b7e0bb0dba23db4
Prabhat Mohanty <mohantyprabhat91@gmail.com>
2023-04-30 17:01:14 +0530

    compo signup

--- demoPro/src/app/app.module.ts
+++ demoPro/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ModalSignupComponent } from './compo/modal-signup/modal-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalSignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ demoPro/src/app/compo/modal-signup/modal-signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-signup',
+  templateUrl: './modal-signup.component.html',
+  styleUrls: ['./modal-signup.component.css']
+})
+export class ModalSignupComponent {
+
+}
